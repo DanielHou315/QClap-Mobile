c5d32bab691aee8836ea634cc820f3e31ac2559b
Daniel Hou <danielhou315@gmail.com>
2022-12-06 21:28:43 -0500

    Initial Commit

---
+++ QClap/QClapApp.swift
@@ -0,0 +1,17 @@
+//
+//  QClapApp.swift
+//  QClap
+//
+//  Created by Daniel Hou on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct QClapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
