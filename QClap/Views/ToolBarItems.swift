a6f5f2f361a50d4c2b7a4547f2aabd4176290a8e
Huaidian Daniel Hou <50754057+DanielHou315@users.noreply.github.com>
2023-06-12 12:18:12 +0800

    Updated Toolbar Items

---
+++ QClap/Views/ToolBarItems.swift
@@ -0,0 +1,20 @@
+//
+//  ToolBarItems.swift
+//  QClap
+//
+//  Created by Daniel Hou on 6/12/23.
+//
+
+import SwiftUI
+
+struct ToolBarItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToolBarItems_Previews: PreviewProvider {
+    static var previews: some View {
+        ToolBarItems()
+    }
+}
